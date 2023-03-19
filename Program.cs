00b94041b32d1c527096c084e5978fc4353bec5b
César Henrique de Figueiredo <cesar.sprite@gmail.com>
2023-03-19 19:10:14 -0300

    comit inicial com classeCesar

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClasseCesar;
+
+Cesar c = new Cesar();
+
+c.escreveNome();
---
+++ classeCesar.cs
@@ -0,0 +1,7 @@
+namespace ClasseCesar {
+    class Cesar {
+        public void escreveNome(){
+            Console.WriteLine("Meu nome é César");
+        }
+    }
+}
