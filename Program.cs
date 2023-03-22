20842d3d2d6f73146350a9f8eed139ddf0376438
17xR14Nx <ryanprocratti@gmail.com>
2023-03-22 13:23:11 -0300

    Commit Ryan

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClasseCesar;
+using ClasseGabriel;
+using ClasseKauan;
+using classeRyan;
+
+Cesar c = new Cesar();
+Gabriel g = new Gabriel();
+Kauan kauan = new Kauan();
+Ryan ry = new Ryan();
+
+kauan.escreveNomeKauan();
+c.escreveNome();
+g.escreveNome();
+ry.escreveNome();
