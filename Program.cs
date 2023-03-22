dd2f0cea9c917d3354df2226238ed68370279aac
Kauan Meira <114404211+kauanmeira@users.noreply.github.com>
2023-03-22 08:29:30 -0300

    Created new Class Kauan

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ClasseCesar;
 using ClasseGabriel;
+using ClasseKauan;
 
 Cesar c = new Cesar();
 Gabriel g = new Gabriel();
+Kauan kauan = new Kauan();
 
+kauan.escreveNomeKauan();
 c.escreveNome();
 g.escreveNome();
